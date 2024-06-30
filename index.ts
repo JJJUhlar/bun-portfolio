1f14579ad65912295b53f89a906b78adc59e0d63
JJJUhlar <jjuhlar@gmail.com>
2024-06-30 14:01:28 +0100

    listen on 0 0 0 0 using Bun boilerplate

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import { Readable } from "stream";
-import { Router } from '@stricjs/router';
 
-export default new Router()
-  .get('/', () =>  new Response(Readable.from(Bun.file('public/index.html').stream())))
-  .get('/ascii', () => new Response(Bun.file("public/ascii.js"), {headers: {"Content-Type": "text/javascript"}}))
-  .get('/frames', () => new Response(Readable.from(Bun.file('public/frames.txt').stream()), {headers: {"Content-Type": "text"}}))
+const app = Bun.serve({
+  port: 3000,
+  hostname: "0.0.0.0",
+  fetch(req) {
+    let route = new URL(req.url).pathname
+    switch (route) {
+      case "/":
+        return new Response(Readable.from(Bun.file('public/index.html').stream()))
+      case "/ascii":
+        return new Response(Bun.file("public/ascii.js"), {headers: {"Content-Type": "text/javascript"}})
+      case "/frames":
+        return new Response(Readable.from(Bun.file('public/frames.txt').stream()), {headers: {"Content-Type": "text"}})
+      default:
+        return new Response("Whoops, nothing to see here!",{status: 404})
+    }
+
+  }
+})
+
+console.log(app.hostname)
+console.log(app.port)
